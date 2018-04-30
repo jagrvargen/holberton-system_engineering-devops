e3f6da1ea89712b7ae33a529ef1d00bbc059f194
jagrvargen <jagarthewolf@gmail.com>
2018-04-30 22:39:25 +0000

    added file 1-export_to_CSV.py with readme update

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+   A script which retrieves employee data from the JSONPlaceholder Rest API and
+   converts it to the CSV format.
+"""
+
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/" + id
+
+    req_name = requests.get(url)
+    req_todos = requests.get(url + "/todos")
+    name = req_name.json()["username"]
+
+    JSON = req_todos.json()
+    task_list = [[] for i in range(len(JSON))]
+
+    for i in range(len(JSON)):
+        task_list[i].append(str(id))
+        task_list[i].append(name)
+        task_list[i].append(str(JSON[i]["completed"]))
+        task_list[i].append(JSON[i]["title"])
+        ",".join(task_list[i])
+
+    print(task_list)
+    with open("{}.csv".format(id), "w") as csv_file:
+        writer = csv.writer(csv_file)
+        for item in task_list:
+            writer.writerow(item)
--- 0x15-api/README.md
+++ 0x15-api/README.md
@@ -1,3 +1,5 @@
 # API
 
 0. 0-gather_data_from_an_API.py - A python script which retrieves data from the JSONPlaceholder Rest API based on an employee ID and prints their name, and the total number of accomplished tasks on their todo list.
+
+1. 1-export_to_CSV.py - Extends the previous script to export data to the CSV format.
