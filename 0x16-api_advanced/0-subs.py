2f9fbbf888ecc9b8d1e75e7d832023d0460d7034
jagrvargen <jagarthewolf@gmail.com>
2018-05-04 13:57:50 +0000

    initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+   This module contains a function which will query the Reddit API and return
+   the total number of subscribers for a given subreddit.
+"""
+import requests
+from sys import argv
+
+
+subreddit = argv[1]
+
+
+def number_of_subscribers(subreddit):
+    query_string = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    user_agent = {"User-Agent": "JDawg's Agent 1.0"}
+
+    req = requests.get(query_string, headers=user_agent)
+    JSON = req.json()
+
+    total_subscribers = JSON["data"]["subscribers"]
+
+    return total_subscribers
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,3 @@
+# API-Advanced
+
+0. 0-subs.py - A function that queries the Reddit API and returns the total number of subscribers for a given subreddit. If an invalid subreddit is given, the function will return 0.
