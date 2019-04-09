27dd95c810966b42a6600ea6cc67d9f393c5248d
jagrvargen <229@holbertonschool.com>
2019-04-09 19:46:29 +0000

    testing puppet file

---
+++ 0x0A-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Configures the /etc/ssh/config file to allow ssh connection
+# w/o password authentication and sets the identity file to
+# ~/.ssh/holberton
+include stdlib
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}->
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#\s+PasswordAuthentication yes',
+}->
+file_line { 'Declare identity file':
+  path  => '/etc/ssh_config',
+  line  => 'IdentityFile ~/.ssh/holberton',
+}
