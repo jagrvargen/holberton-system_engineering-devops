a8a767d2c66f3fc8994711169730c785d202b9eb
jagrvargen <jesse.hedden@holbertonschool.com>
2019-04-19 17:45:03 +0000

    adding file to test checker

---
+++ 0x0F-load_balancer/2-custom_http_response-header.pp
@@ -0,0 +1,19 @@
+# Answer file for 0x0F - Load Balancer
+# Sets a custom http header response for an Nginx web server
+include stdlib
+exec { 'update':
+  command  => '/usr/bin/apt update',
+}->
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
+}->
+file { 'ensure default':
+  path     => '/etc/nginx/sites-enabled/default',
+  ensure   => present,
+}->
+file_line { 'add custom header':
+  path     => '/etc/nginx/sites-enabled/default',
+  line     => "server { \n        add_header X-Served-By \$hostname; \n ",
+  match    => "^server {",
+}
