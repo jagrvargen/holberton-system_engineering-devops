5c2ee6d9ee86bfa3d40bd1e97ca55aa61baea5c1
jagrvargen <229@holbertonschool.com>
2019-06-04 23:08:04 +0000

    adding file to test checker

---
+++ 0x0B-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,15 @@
+# Installs and configures nginx
+include stdlib
+package { 'install nginx':
+  name   => 'nginx',
+  ensure => 'present',
+}->
+file_line { 'add Holberton School to index.html':
+  path => '/usr/share/nginx/html/index.html',
+  line => 'Hello Holberton',
+}->
+file_line { 'Add 301 redirect':
+  path  => '/etc/nginx/sites-available/default',
+  match  => "^server\s+{\n",
+  line => "server {\n\tlocation /redirect_me {\n\t\trewrite ^/.*$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n}\n",
+}
